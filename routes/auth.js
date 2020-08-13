f3d91f138d7443d96f0a92d2c7c201376097ebe6
yoel <yoel@MacBook-Pro.local>
2020-08-13 14:03:11 -0700

    [auth] add post route + some validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
-router.get('/signup', (req, res) => {
-  res.render('auth/signup');
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
 });
 
-router.get('/login', (req, res) => {
-  res.render('auth/login');
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post("/signup", (req, res) => {
+  db.user
+    .findOrCreate({
+      where: {
+        email: req.body.email,
+      },
+      defaults: {
+        name: req.body.name,
+        password: req.body.password,
+      },
+    })
+    .then(([user, created]) => {
+      if (created) {
+        // if created, success and redirect to home
+        console.log(`${user.name} was created`);
+        res.redirect("/");
+      } else {
+        // Email already exist
+        console.log("Email already exist");
+        res.redirect("/auth/signup");
+      }
+    })
+    .catch((error) => {
+      console.log("Error", error);
+      res.redirect("/auth/signup");
+    });
 });
 
 module.exports = router;
